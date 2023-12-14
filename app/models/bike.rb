0d21ccecb03e0a3ade217c5552f6f2b1f02fe718
henri guillot <henri.guillot@hotmail.com>
2023-12-14 12:50:32 +0100

    models

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,10 @@
 class Bike < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_one_attached :photo, dependent: :destroy
+
+  validates :name, :type, :address, :price_per_day, :description, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 2 }
+  validates :description, length: { minimum: 5 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings, dependent: :destroy
+  has_many :vehicles, dependent: :destroy
+
+  has_one_attached :photo, dependent: :destroy
 end
