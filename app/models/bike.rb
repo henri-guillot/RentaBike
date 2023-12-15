1ac42012cc26d04335dd060b7cb9f98929ab8654
henri guillot <henri.guillot@hotmail.com>
2023-12-15 08:03:52 +0100

    front index

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_one_attached :photo, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, :bike_type, :place, :price_per_day, :description, presence: true
   validates :name, uniqueness: true
